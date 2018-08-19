b63684331edc9d52ca30fe3a1bd2cbe5dcc72ff4
SavorTheFlavor <2500364530@qq.com>
2018-08-19 16:36:08 +0800

    finish 'like' component

--- components/like/index.js
+++ components/like/index.js
@@ -4,16 +4,25 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    like:{
+      type: Boolean,
+      value: false
+    },
+    count:{
+      type: Number
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-    like: true,
-    count1: 99,
-    count2: 0
+    //数据绑定
+    //封装性 开放性
+    //哪些是被封装在内部的，哪些是开放出来给外部设置的
+    //开放出来的data就叫做properties
+    likedSrc: 'images/like.png',
+    dislikedSrc: 'images/like@dis.png'
   },
 
   /**
@@ -21,7 +30,15 @@ Component({
    */
   methods: {
     onLike: function(event){
-      console.log(event)
+      // let variable scope ---> '{....}'
+      let like = this.properties.like
+      let count = this.properties.count
+      count = like?count-1:count+1
+      // the setData's 'Data' is the data above
+      this.setData({
+        like:!like,
+        count:count
+      })
     }
   }
 })
